09bc5ecaa8b7c36e203053def360ea06bde70eb7
DonKiwi-cyber <minecraft200294@gmail.com>
2024-03-08 10:49:59 -0600

    empty springboot

---
+++ src/main/java/org/uv/dapp01tarea02/Dapp01Tarea02Application.java
@@ -0,0 +1,13 @@
+package org.uv.dapp01tarea02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dapp01Tarea02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dapp01Tarea02Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/uv/dapp01tarea02/Dapp01Tarea02ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.uv.dapp01tarea02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dapp01Tarea02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
