d6d2b36fb46d06fa2100ce06435ae1c4e5d2cb51
Francisco <fernandezfrancisco261@gmail.com>
2024-03-14 19:19:15 -0600

    metodos put

--- src/main/java/org/uv/dapp02practica2/DAPP02Practica2.java
+++ src/main/java/org/uv/dapp02practica2/DAPP02Practica2.java
@@ -2,7 +2,14 @@ package org.uv.dapp02practica2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -14,9 +21,40 @@ public class DAPP02Practica2 {
 
     @RestController
     public static class HelloWorldController {
-        @GetMapping("/")
+ 
+        
+     @GetMapping("/")
         public String helloWorld() {
-            return "¡Hola Mundo!";
+            return "Hello World!";
         }
+
+        @GetMapping("/msg")
+        public DTOUsuario msg() {
+            return new DTOUsuario("francisco", "calle 1");
+        }
+
+        @GetMapping("/msg/{id}")
+        public DTOUsuario msg(@PathVariable("id") Long id) {
+            return new DTOUsuario("francisco" + id, "calle 1" + id);
+        }
+
+        @PostMapping("/msg")
+        public String msg2(@RequestBody DTOUsuario usr) {
+            String msg = "se recibio " + usr.getNombre() + "--" + usr.getDireccion() + "--" + usr.getTelefono();
+            return "{ \"msg\": \"" + msg + "\" }";
+        }
+
+        @PutMapping("/msg")
+        public String msg3(@RequestBody DTOUsuario usr) {
+            String msg = "se editó " + usr.getNombre() + "--" + usr.getDireccion() + "--" + usr.getTelefono();
+            return "{ \"msg\": \"" + msg + "\" }";
+        }
+
+        @DeleteMapping("/msg/{id}")
+        public String msg4(@PathVariable Long id) {
+            String msg = "se eliminó el empleado con ID: " + id;
+            return "{ \"msg\": \"" + msg + "\", \"id\": " + id + " }";
+        }
+
     }
 }
